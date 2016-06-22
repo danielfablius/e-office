3bad83c6adcf75e1a6b5167c773147d0318388d9
admiral budi arviansyah <arvi.admiral@gmail.com>
2016-06-22 09:21:28 +0700

    tambah file pengguna

---
+++ BackEnd+DataBase(Data)/routes/pengguna.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+
+
+//select all jabatan Done(that is_delete not 1)
+router.get('/pengguna', function(req, res, next) {
+  	var db = req.db;
+  	var collection = db.get('pengguna');
+  	collection.find({},{},function(err,docs){
+  		res.json({"pengguna" : docs});
+  });  
+});
+
+//detail pengguna
+
+//insert into pengguna
+router.post('/pengguna', function(req, res){
+	var db = req.db;
+
+	var username		= req.body.username;
+	var password		= req.body.password;
+	var nama			= req.body.nama;
+	var id_jabatan		= req.body.id_jabatan;
+	var no_telp			= req.body.no_telp;
+	var email			= req.body.email;
+	var detail_jabatan	= req.body.detail_jabatan;
+	var is_delete		= "0";
+
+	var collection = db.get('pengguna');
+
+	collection.insert({
+		"username"	: username,
+		"nama"		: nama,
+		"no_telp"	: no_telp,
+		"email"		: email,
+		"is_delete"	: is_delete
+	}, 
+		db.leftCollection.aggregate([{
+			$lookup:
+			{
+				from:"jabatan",
+				localField: : "id_jabatan",
+				foreignField: "id_jabatan",
+				as: "embeddedData"
+			}
+		}]),
+	function (err, doc){
+		if(err){
+			res.json({message: 'insert failed'});
+		}
+		else {
+			res.json({message: 'insert success'});
+		}
+	});
+});
+
+//update pengguna
+router.put('/pengguna/:id_pengguna', function(req, res){
+	var db = req.db;
+
+	var username	= req.body.username;
+	var password	= req.body.password;
+	var nama		= req.body.nama;
+	var id_jabatan	= req.body.id_jabatan;
+	var no_telp		= req.body.no_telp;
+	var email		= req.body.email;
+	var is_delete	= req.body.is_delete;
+
+	var collection	= db.get('pengguna');
+
+	collection.update({
+		"username"	: username,
+		"nama"		: nama,
+		"no_telp"	: no_telp,
+		"email"		: email,
+		"is_delete"	: is_delete
+	}, function (err, doc){
+		if(err){
+			res.json({message: 'update failed'});
+		}
+		else{
+			res.json({message: 'update success'});
+		}
+	});
+});
+
+//delete pengguna
+router.delete('/pengguna/:id_pengguna', function(req, res,next){
+	var db			= req.db;
+	var is_delete	= "1";
+
+	var collection	= db.get('pengguna');
+	collection.update(
+	{"_id" : req.params.agenda_id},
+	{$set: {"is_delete" : is_delete}},
+	function(err,docs){
+		if(err){
+			res.json({message: 'delete failed'});
+		}
+		else{
+			res.json({message: 'delete success'});
+		}
+	});
+});
